04897bc321a342da1b18773787dd2ec0f53881d4
cyntler <damian@cyntler.com>
2021-03-25 17:13:06 +0100

    Add util for errors logging

---
+++ src/utils/logError.ts
@@ -0,0 +1,15 @@
+/**
+ * @name react-coming
+ * @author cyntler <damian@cyntler.com>
+ */
+import packageJson from '../../package.json';
+
+const { name, version } = packageJson;
+
+export const logError = (message: string) => {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  console.error(`${name} v.${version}: ${message}`);
+};
