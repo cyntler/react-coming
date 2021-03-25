274e4b904be601f2c9b4178a4a2b58ff82827c9f
cyntler <damian@cyntler.com>
2021-03-25 17:14:01 +0100

    Add util to calculate time values

---
+++ src/utils/calculateValues.ts
@@ -0,0 +1,22 @@
+/**
+ * @name react-coming
+ * @author cyntler <damian@cyntler.com>
+ */
+const calculateDaysValue = (distance: number) =>
+  Math.floor(distance / (1000 * 60 * 60 * 24));
+
+const calculateHoursValue = (distance: number) =>
+  Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+const calculateMinutesValue = (distance: number) =>
+  Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+const calculateSecondsValue = (distance: number) =>
+  Math.floor((distance % (1000 * 60)) / 1000);
+
+export const calculateValues = (distance: number) => [
+  calculateDaysValue(distance),
+  calculateHoursValue(distance),
+  calculateMinutesValue(distance),
+  calculateSecondsValue(distance),
+];
