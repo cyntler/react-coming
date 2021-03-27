2807ed4d390694da9e2115b8a075d6e449e82794
cyntler <damian@cyntler.com>
2021-03-27 21:44:40 +0100

    Add a simple script to inject font css file

---
+++ src/utils/injectFont.ts
@@ -0,0 +1,7 @@
+export const injectFont = () => {
+  const link = document.createElement('link');
+  link.href =
+    'https://fonts.googleapis.com/css2?family=Merriweather&amp;amp;subset=latin-ext';
+  link.rel = 'stylesheet';
+  document.head.appendChild(link);
+};
