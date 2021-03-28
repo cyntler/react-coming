d35036c314ef9fe1dac06ca4ae04c8adfb3dfabb
cyntler <damian@cyntler.com>
2021-03-28 14:12:27 +0200

    Update log error util

--- src/utils/logError.ts
+++ src/utils/logError.ts
@@ -2,14 +2,10 @@
  * @name react-coming
  * @author cyntler <damian@cyntler.com>
  */
-import packageJson from '../../package.json';
-
-const { name } = packageJson;
-
 export const logError = (message: string) => {
   if (process.env.NODE_ENV !== 'development') {
     return;
   }
 
-  console.error(`${name}: ${message}`);
+  console.error(`react-coming: ${message}`);
 };
